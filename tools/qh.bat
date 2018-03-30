21c0fa056338c09168ebeb1263f4748124dfc73d
Michel <michelc@gmail.com>
2018-03-30 12:19:07 +0200

    Ajout chapitres vides privas.

--- tools/qh.bat
+++ tools/qh.bat
@@ -1,3 +1,9 @@
 @echo off
 
-C:\tools\QuickHtml\QuickHtml.exe ..
+if exist qh.bat C:\tools\QuickHtml\QuickHtml.exe ..
+if not exist qh.bat C:\tools\QuickHtml\QuickHtml.exe
+
+REM if %0 == "%~0"  pause
+
+echo %cmdcmdline% | find /i "%~0" > nul
+if not errorlevel 1 pause
